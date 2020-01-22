1199a65df01cb008a647ac22757f58d44a8abec4
Gabriel Castello <gabriel__castello@hotmail.com>
2020-01-22 10:42:56 -0300

    updated jest test to trigger git hook on jenkins

--- __tests__/unit/unit.test.js
+++ __tests__/unit/unit.test.js
@@ -1,7 +1,13 @@
 describe("basic unit test", () => {
-  it(`should require basicAuth`, async () => {
+  it(`should expect the sum of 1+1 to be 2`, async () => {
     const sum = 1 + 1;
 
     expect(sum).toBe(2);
+  });
+
+  it(`should expect the sum of 1+1 not to be 1`, async () => {
+    const sum = 1 + 1;
+
+    expect(sum).not.toBe(1);
   });
 });
